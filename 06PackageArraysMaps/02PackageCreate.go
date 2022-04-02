668307a1400656264b8173678bbfd3d174971e5e
framazanyilmaz <framazanyilmaz@gmail.com>
2022-04-03 01:42:43 +0300

    PackageArray

---
+++ 06PackageArraysMaps/02PackageCreate.go
@@ -0,0 +1,10 @@
+package main
+
+func main() {
+
+	//Paketlerde bulunan variablelar ve func ların başlangıç harfi büyük harf ile başlar
+
+	//Workspace de kullanacğaımız metotların bulunduğu bir folder oluştuurp onun içinde .go
+	//uzantılı bir şekilde kodlarımızı yazabiliriz.
+
+}
